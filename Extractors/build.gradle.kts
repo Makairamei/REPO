b41f4a4cbb900ee48eeb99812cdc91a28489e078
sad25kag <sad25kag@gmail.com>
2026-05-13 08:39:43 +0700

    add

---
+++ Extractors/build.gradle.kts
@@ -0,0 +1,19 @@
+version = 11
+
+cloudstream {
+    authors = listOf("IstarVin")
+    language = "en"
+    description = "Extractors for my sources"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     **/
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf("Others")
+
+    isCrossPlatform = true
+}
