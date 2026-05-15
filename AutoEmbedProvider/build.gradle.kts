e9d32b93e77bf1ceeb244b3f6bd3d6db884326b9
sad25kag <sad25kag@gmail.com>
2026-05-15 09:05:51 +0700

    add

---
+++ AutoEmbedProvider/build.gradle.kts
@@ -0,0 +1,14 @@
+version = 1
+
+cloudstream {
+    language = "en"
+    authors = listOf("BetbetMiro")
+    description = "AutoEmbed TMDB-backed movie and TV catalog provider"
+    status = 1
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+    )
+    iconUrl = "https://www.themoviedb.org/favicon.ico"
+    isCrossPlatform = true
+}
