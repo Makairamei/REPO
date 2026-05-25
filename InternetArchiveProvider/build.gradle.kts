722d52938921cca50d283d240120763ea40a2982
sad25kag <sad25kag@gmail.com>
2026-05-25 10:29:11 +0700

    chore(provider): update language and region settings

--- InternetArchiveProvider/build.gradle.kts
+++ InternetArchiveProvider/build.gradle.kts
@@ -1,5 +1,5 @@
 // Use an integer for version numbers
-version = 2
+version = 3
 
 cloudstream {
     description = "Watch movies, classic TV, animation, and public-domain video from the Internet Archive"
