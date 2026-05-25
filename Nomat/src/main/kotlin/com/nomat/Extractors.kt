38b7a889a8394258429e0dfcbac2373dbb744c05
sad25kag <sad25kag@gmail.com>
2026-05-25 23:36:00 +0700

    package com.nomat
    import com.lagradost.api.Log
    import com.lagradost.cloudstream3.app
    import com.lagradost.cloudstream3.extractors.VidHidePro
    
    class Hydrax: VidHidePro() {
        override var name = "Hydrax"
        override var mainUrl = "https://playhydrax.com"
    }

--- Nomat/src/main/kotlin/com/nomat/Extractors.kt
+++ Nomat/src/main/kotlin/com/nomat/Extractors.kt
@@ -4,8 +4,6 @@ import com.lagradost.api.Log
 import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.extractors.VidHidePro
 
-
-
 class Hydrax: VidHidePro() {
     override var name = "Hydrax"
     override var mainUrl = "https://playhydrax.com"
